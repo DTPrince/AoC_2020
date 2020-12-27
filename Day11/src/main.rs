a527950b9fcf884d2ec066c8535cbe74e5dccbb0
DTPrince <aaoeaaoe08>
2020-12-27 16:50:24 -0700

    Started making some real wild shit but about to scrap it

--- Day11/src/main.rs
+++ Day11/src/main.rs
@@ -3,11 +3,17 @@
  * Seats, please
  */
 
+extern crate petgraph;
+
 use std::fs::File;
 use std::io::{Error, BufReader, BufRead};
 use std::collections::HashMap;
+use petgraph::graphmap::{UnGraphMap};
 
-#[derive(Copy, Clone, Debug)]
+const ROW_WIDTH: usize = 95;   // change as needed for test cases and what-have-you
+
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum State { // L, #, .
     Empty,
     Occupied,
@@ -18,15 +24,20 @@ impl Default for State {
     fn default() -> Self { State::Empty }
 }
 
-#[derive(Copy, Clone, Default, Debug)]
+// Scary derives
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Position {
-    seat: State,
     id: usize,
+    seat: State,
+    row: usize,
+    column: usize,
 }
 
-// impl Position {
-//
-// }
+impl Position {
+    fn as_id(row: usize, column: usize) -> usize {
+        (row*ROW_WIDTH) + column
+    }
+}
 
 fn read_input() -> Result<Vec<String>, Error> {
     let path = "./input/input.txt";
@@ -34,12 +45,16 @@ fn read_input() -> Result<Vec<String>, Error> {
     let input = File::open(path)?;
     let buffered = BufReader::new(input);
 
-    let lines = buffered.lines().map(|l| l.unwrap()).collect();
+    let lines: Vec<String> = buffered.lines().map(|l| l.unwrap()).collect();
+    // println!("len: {}", lines.first().unwrap().len());   // checking len for wrap conditions
     Ok(lines)
 }
 
 fn create_map(fcontents: Vec<String>) -> HashMap<usize, Position> {
+    // The fact that I can only store numbers as nodes is beyond infuriating.
+    // Let me reference m'fuckin' structs with an ID and associate edges.
     let mut map: HashMap<usize, Position> = HashMap::new();
+    let mut neighbors = UnGraphMap::<usize, ()>::new();
 
     for (num, line) in fcontents.iter().enumerate() {
         let pos = line.as_str();
@@ -51,24 +66,51 @@ fn create_map(fcontents: Vec<String>) -> HashMap<usize, Position> {
                 '.' => seat.seat = State::Floor,
                 _ => println!("Somehow got a miss-matched state. Consider cleaning your input."),
             }
-            map.insert(num+offset, seat);
+            seat.row = num;
+            seat.column = offset;
+            seat.id = (num*ROW_WIDTH)+offset;
+            map.insert(seat.id, seat);
+            neighbors.add_node(seat.id);
         }
+    }
+    // gmap.contains_node(96);
+    //associate edges
+    for seat in 0..map.len() {
+        let center = map.get(&seat).unwrap();
+
+        let adjacents = vec![
+            Position::as_id(center.row - 1, center.column - 1), Position::as_id(center.row - 1, center.column), Position::as_id(center.row - 1, center.column + 1),
+            Position::as_id(center.row, center.column - 1), Position::as_id(center.row, center.column), Position::as_id(center.row, center.column + 1),
+            Position::as_id(center.row + 1, center.column - 1), Position::as_id(center.row + 1, center.column), Position::as_id(center.row + 1, center.column + 1)];
+
+
     }
     map
 }
 
-fn seat_people(mut seat_map : &HashMap<usize, Position>) -> bool {
-    let seating_changed = true;
-    
+fn seat_people(seat_map : &mut HashMap<usize, Position>) -> bool {
+    let seating_changed = false;
+    let map_len = seat_map.len();
+    for seat in 0..map_len {
+        let current = seat_map.get_mut(&seat).unwrap();
+        match current.seat {
+            State::Empty => {
 
-
+            }
+            State::Occupied => {}
+            State::Floor => {}
+        }
+    }
+    // for (key, val) in seat_map.iter_mut() {
+    //     println!("Key: {}, Val: R{}C{}", key, val.row, val.column);
+    // }
     seating_changed
 }
 
 fn main() -> Result<(), Error> {
     let fcontents = read_input()?;
     let mut seat_map : HashMap<usize, Position> = create_map(fcontents);
-    while seat_people(&seat_map) {}
+    while seat_people(&mut seat_map) {}
 
     Ok(())
 }
